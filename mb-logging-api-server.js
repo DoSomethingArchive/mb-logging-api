3f9ae4dc4d2d47e7748e7ca41fe3762857104d40
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-14 23:35:54 -0400

    Restore Mongoose schema definition details

--- mb-logging-api-server.js
+++ mb-logging-api-server.js
@@ -113,9 +113,115 @@ app.listen(port, function() {
 // =============================================================================
 // All configurations related to the mb-logging Mongo database.
 if (app.get('env') == 'production') {
-  var database = mb_config.mongo.production;
+  var mongoUri = mb_config.mongo.production;
 }
 else {
-  var database = mb_config.mongo.development;
+  var mongoUri = mb_config.mongo.development;
 }
-mongoose.connect(database);
+mongoose.connect(mongoUri);
+mongoose.connection.on('error', function(err) {
+  console.log('Unable to connect to the Mongo database (%s). Check to make sure the database is running.', mongoUri);
+  process.exit();
+});
+
+// Define schema / models
+var userImportModel;
+var userImportCollectionName_Niche = 'userimport-niche';
+var userImportCollectionName_HerCampus = 'userimport-hercampus';
+var userImportCollectionName_ATT_iChannel = 'userimport-att-ichannel';
+var userImportCollectionName_TeenLife = 'userimport-teenlife';
+var importSummaryModel;
+var importSummaryCollectionName = 'import-summary';
+
+var externalApplicationUserEventModel;
+var externalApplicationUserEventCollectionName = 'externalapplicationuserevent';
+
+mongoose.connection.once('open', function() {
+
+  // User import logging schema for existing entries
+  var userImportLoggingSchema = new mongoose.Schema({
+    logged_date : { type: Date, default: Date.now },
+    origin : {
+      name : { type : String, trim : true },
+      processed : { type: Date }
+    },
+    source : {
+      type : String,
+      lowercase : 1,
+      trim : true,
+      enum: ['niche', 'niche.com', 'hercampus', 'att-ichannel', 'teenlife']
+    },
+    phone : {
+      number : { type : String, trim : true },
+      status : { type : String, trim : true },
+      acquired : { type: Date }
+    },
+    email : {
+      address : { type : String, trim : true },
+      status : { type : String, trim : true },
+      acquired : { type: Date }
+    },
+    drupal : {
+      email : { type : String, trim : true },
+      uid : { type : Number }
+    }
+  });
+  userImportLoggingSchema.set('autoIndex', false);
+
+  // Logging model
+  userImportModel_niche = mongoose.model(userImportCollectionName_Niche, userImportLoggingSchema);
+  userImportModel_hercampus = mongoose.model(userImportCollectionName_HerCampus, userImportLoggingSchema);
+  userImportModel_att_ichannel = mongoose.model(userImportCollectionName_ATT_iChannel, userImportLoggingSchema);
+  userImportModel_teenlife = mongoose.model(userImportCollectionName_TeenLife, userImportLoggingSchema);
+
+  // User import logging schema for summary reports
+  var importSummarySchema = new mongoose.Schema({
+    logged_date : { type: Date, default: Date.now },
+    target_CSV_file : { type : String, trim : true },
+    signup_count : { type : Number },
+    skipped : { type : Number },
+    source : {
+      type : String,
+      lowercase : 1,
+      trim : true,
+      enum: ['niche', 'niche.com', 'hercampus', 'att-ichannel', 'teenlife']
+    },
+    log_type : {
+      type : String,
+      lowercase : 1,
+      trim : true,
+      enum: ['user_import']
+    },
+  });
+  importSummarySchema.set('autoIndex', false);
+
+  // Logging summary model
+  importSummaryModel = mongoose.model(importSummaryCollectionName, importSummarySchema);
+
+  // External application user event logging schema for "in the future" email list generation
+  var externalApplicationUserEventSchema = new mongoose.Schema({
+    logged_date : { type: Date, default: Date.now },
+    email : { type : String, trim : true },
+    source : {
+      type : String,
+      lowercase : 0,
+      trim : true,
+      enum: ['CGG', 'AGG']
+    },
+    activity : {
+      type : String,
+      lowercase : 1,
+      trim : true,
+      enum: ['vote']
+    },
+    activity_date : { type: Date, default: Date.now },
+    activity_timestamp : { type : Number },
+    activity_details : { type : String }
+  });
+  externalApplicationUserEventSchema.set('autoIndex', false);
+
+  // Logging summary model
+  externalApplicationUserEventModel = mongoose.model(externalApplicationUserEventCollectionName, externalApplicationUserEventSchema);
+
+  console.log("Connection to Mongo (%s) succeeded! Ready to go...\n\n", mongoUri);
+});
